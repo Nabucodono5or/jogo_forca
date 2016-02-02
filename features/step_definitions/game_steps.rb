2316a61fb22bac980d2ba77bff20a1973af9c656
jefferson abreu <nabucodonosor310@gmail.com>
2016-02-01 22:42:20 -0200

    definindo os steps

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,13 +1,21 @@
 # encoding: UTF-8
 
 Quando /^começo um novo jogo/ do
-	steps %{
-	When I run 'forca' interactively
-	}
+	start_new_game
 end
 
 Então /^vejo na tela:$/ do |string|
 	steps %{
 		Then the stdout should contain "#{string}"	
 	}
+end
+
+Dado /^que comecei um jogo$/ do
+	start_new_game
+end
+
+Quando /^escolho que a palavra sorteada deverá ter "(.*?)" letras\$/ do |number_of_letters|
+	steps %{
+		When I type "#{number_of_letters}"
+	} 
 end
---
+++ features/support/game_helpers.rb
@@ -0,0 +1,10 @@
+module GameHelpers
+    def start_new_game
+        steps %{
+           When I run 'forca' interactively
+        }            
+    end
+end
+
+
+World(GameHelpers)
