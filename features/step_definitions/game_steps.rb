e8a0e13e553ac0c00b2af004776cbdb60d0a4bc6
Jefferson Abreu <nabucodonosor310@gmail.com>
2016-02-03 13:23:14 -0200

    tentando capturar erro de status exit

--- features/step_definitions/game_steps.rb
+++ features/step_definitions/game_steps.rb
@@ -1,21 +1,26 @@
 # encoding: UTF-8
 
-Quando /^começo um novo jogo/ do
-	start_new_game
-end
-
-Então /^vejo na tela:$/ do |string|
-	steps %{
-		Then the stdout should contain "#{string}"	
-	}
-end
-
 Dado /^que comecei um jogo$/ do
 	start_new_game
 end
 
-Quando /^escolho que a palavra sorteada deverá ter "(.*?)" letras\$/ do |number_of_letters|
+Quando /^começo um novo jogo/ do
+	start_new_game
+end
+
+
+Quando /^escolho que a palavra sorteada deverá ter "(.*?)" letras$/ do |number_of_letters|
 	steps %{
 		When I type "#{number_of_letters}"
 	} 
 end
+
+Então /^o jogo termina com a seguinte mensagem na tela:$/ do |text|
+	steps %{
+		Then the output should contain exactly:
+		"""
+		#{text}
+		"""	
+	}
+
+end 
