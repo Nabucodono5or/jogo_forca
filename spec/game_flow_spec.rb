ec88a6477354004474f3116cb933a03a8dfd3a9c
Jefferson Abreu <nabucodonosor310@gmail.com>
2016-03-28 10:14:10 -0300

    conserto do primeiro erro de sintaxe rspec

--- spec/game_flow_spec.rb
+++ spec/game_flow_spec.rb
@@ -5,6 +5,7 @@ require 'game_flow'
 
 describe GameFlow do
 	let(:ui) { double("ui").as_null_object }
+	let(:word_raffler) { double("raffler").as_null_object }
 	let(:game) { double("game",state: :initial, guess_letter: []).as_null_object }
 
 	subject(:game_flow) { GameFlow.new(game,ui) }
@@ -40,12 +41,13 @@ describe GameFlow do
 				game_flow.next_step			
 			end
 
+
 			context "and the player asks to raffle a word" do
 				it "raffles a word with the given length" do
 					word_length = "3"
 					allow(ui).to receive(:read).and_return(word_length)
 
-					expect(game).to receive(:raffle)
+					expect(game).to receive(:raffle).with(word_length.to_i)
 				
 					game_flow.next_step			
 				end
